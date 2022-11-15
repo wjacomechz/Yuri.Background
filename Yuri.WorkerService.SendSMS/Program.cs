c453cc41f4af0a6a08b9485d8c8fca2c54fd22b1
wjacomechz <wjacomechz@gmail.com>
2022-11-14 23:15:31 -0500

    subida inicial proyecto segundo plano de YURI

---
+++ Yuri.WorkerService.SendSMS/Program.cs
@@ -0,0 +1,10 @@
+using Yuri.WorkerService.SendSMS;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
